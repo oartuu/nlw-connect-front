f066c47076587e7cba636c2882a4dab6c1d217af
Arthur <artu.donascimento@gmail.com>
2025-02-19 00:18:22 -0300

    feat: making pages components and forms validadions

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,8 @@
 import Image from 'next/image';
 
-import Logo from '../assets/Logo.svg'
-import { ArrowRight, Mail, Radio, User } from 'lucide-react';
-import { InputField, InputIcon, InputRoot } from '@/components/Input';
-import { Button } from '@/components/Button';
+import Logo from '../../assets/Logo.svg'
+import { Radio } from 'lucide-react';
+import SubscriptionForm from './subscription-form';
 
 export default function Home() {
   return (
@@ -30,29 +29,9 @@ export default function Home() {
               <br />
               Dias 15 a 17 de março | Das 18h às 21h | Online & Gratuito </p>
           </div>
-          <form className='bg-gray-700 border-gray-600 rounded-2xl p-8 space-y-6 w-full md:max-w-[440px]' action="">
-            <h2 className='font-heading font-semibold text-gray-200 text-xl'>Inscrição </h2>
-            <div className='space-y-3'>
-              <InputRoot>
-                <InputIcon>
-                  <User/>
-                </InputIcon>
-                <InputField type='text' placeholder="Nome completo" />
-              </InputRoot>
+          
+          <SubscriptionForm />
 
-              <InputRoot>
-                <InputIcon>
-                  <Mail/>
-                </InputIcon>
-                <InputField type='text' placeholder="E-mail" />
-              </InputRoot>
-
-            </div>
-            <Button type='submit'>
-              Confirmar 
-              <ArrowRight/>
-            </Button>
-          </form>
         </div>
       </div>
   );
--- src/app/invite/page.tsx
+++ src/app/invite/page.tsx
@@ -1,17 +1,21 @@
 import Image from "next/image";
 
 import Logo from '../../assets/Logo.svg'
-import gold from '../../assets/Medal-gold.svg'
-import silver from '../../assets/Medal-silver.svg'
-import cooper from '../../assets/Medal-cooper.svg'
+
 
 import { InputField, InputIcon, InputRoot } from "@/components/Input";
 import { BadgeCheck, Copy, Link, Medal, MousePointerClick } from "lucide-react";
 import { IconButton } from "@/components/IconButton";
+import { Ranking } from "./ranking";
+import { Stats } from "./stats";
+import InviteLinkInput from "./invite-link-input";
 
 
 
 export default function InvitePage() {
+
+  const inviteLink =  "https://localhost:3000/invite/904982304820854"
+
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[500px]">
@@ -37,101 +41,15 @@ export default function InvitePage() {
             </p>
           </div>
 
-          <InputRoot>
-            <InputIcon>
-              <Link />
-            </InputIcon>
-            <InputField
-              readOnly
-              defaultValue={"htttp://localhost:3000/invite/084298502958720"}
-            />
+          <InviteLinkInput inviteLink={inviteLink}/>
 
-            <IconButton className="-mr-2">
-              <Copy className="size-5" />
-            </IconButton>
-          </InputRoot>
+          <Stats/>
 
-          <div className="grid gap-3 md:grid-cols-3 ">
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justfy-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1042
-              </span>
-              <span className="text-sm text-gray-300 text-center leading-none">
-                Acessos ao link
-              </span>
-              <MousePointerClick className="size-5 text-purple absolute top-3 left-3 " />
-            </div>
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justfy-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                1042
-              </span>
-              <span className="text-sm text-gray-300 text-center leading-none">
-                Inscrições feitas
-              </span>
-              <BadgeCheck className="size-5 text-purple absolute top-3 left-3 " />
-            </div>
-            <div className="relative bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justfy-center gap-1 rounded-xl">
-              <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-                3º
-              </span>
-              <span className="text-sm text-gray-300 text-center leading-none">
-                Posição no ranking
-              </span>
-              <Medal className="size-5 text-purple absolute top-3 left-3 " />
-            </div>
-          </div>
         </div>
       </div>
 
-      <div className="w-full max-w-[440px] space-y-5">
-        <h2 className="text-xl text-gray-200 font-heading font-semibold leading-none">
-          Ranking de indicaçoes
-        </h2>
-        <div className="space-y-4">
-          <div className=" relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justfy-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">1º</span> | Arthur
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-            <Image
-              src={gold}
-              alt="medalha de ouro"
-              
-              className="absolute top-0 right-8"
-            />
-          </div>
-          <div className=" relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justfy-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">1º</span> | Arthur
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-            <Image
-              src={silver}
-              alt="medalha de ouro"
-              
-              className="absolute top-0 right-8"
-            />
-          </div>
-          <div className=" relative rounded-xl bg-gray-700 border border-gray-600 p-6 flex flex-col justfy-center gap-3">
-            <span className="text-sm text-gray-300 leading-none">
-              <span className="font-semibold">1º</span> | Arthur
-            </span>
-            <span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
-              1030
-            </span>
-            <Image
-              src={cooper}
-              alt="medalha de ouro"
-            
-              className="absolute top-0 right-8"
-            />
-          </div>
-        </div>
-      </div>
+        <Ranking/>
+     
     </div>
   );
 }
